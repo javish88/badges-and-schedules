6390f908b480f9cb76bf17702c94d6d65cfb0a02
Javier <tsakasu88@gmail.com>
2021-01-24 16:19:56 -0500

    commit tree f55dc9c50ed9b0003c89c93d01794254f49fa064

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.map {|attendee| "Hello, my name is #{attendee}."}
+end
+
+def assign_rooms(attendees)
+    newArrayTwo = []
+    room_assignments = 1
+    attendees.each do |attendee|
+        newArrayTwo << "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
+        room_assignments += 1
+    end
+    newArrayTwo
+end
+
+def printer(attendees)
+        attendees.map do |attendee|
+            puts "Hello, my name is #{attendee}."
+        end
+        room_assignments = 1  
+        attendees.each do |attendee|
+            puts "Hello, #{attendee}! You'll be assigned to room #{room_assignments}!"
+            room_assignments += 1         
+    end
+end 
